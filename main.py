d136444bc90f288b9bf36bb8dbb1fe09ef43caf0
StandB <standb@protonmail.com>
2020-09-23 15:25:03 +0200

    save IP and checkbox settings

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import PySimpleGUI as sg
 
 INSTALL_TYPE = "CHOCO"
 
+ip = ''
+borderless = False
+
+if (os.path.isfile('settings.txt')):
+    with open('settings.txt') as f:
+        ip = f.readline().rstrip()
+        borderless = f.readline() == "True"
+
+
+
 sg.theme('DarkBlue3')	# Add a touch of color
-def launch(ip):
+def launch(ip,borderless):
     adb_binary = "adb"
     scrcpy_binary = "scrcpy"
     if INSTALL_TYPE == "LOCAL":
@@ -14,10 +24,14 @@ def launch(ip):
         pass
     else:
         DETACHED_PROCESS = 0x08000000
-        subprocess.Popen([f"{adb_binary}", "connect", f"{ip}:5555"], creationflags=DETACHED_PROCESS)
-        subprocess.Popen([f"{scrcpy_binary}", "-s", f"{ip}:5555", "--window-borderless", "-w", "-b2M", "-m800"], creationflags=DETACHED_PROCESS)
+        subprocess.Popen([f"{adb_binary}", "connect", f"{ip}:5555"])
+        args = [f"{scrcpy_binary}", "-s", f"{ip}:5555", "-w", "-b2M", "-m800"]
+        if borderless:
+            args.append("--window-borderless")
+        cmd = subprocess.Popen(args, creationflags=DETACHED_PROCESS)
 
-layout = [  [sg.Text('Phone IP'), sg.InputText()],
+layout = [  [sg.Text('Phone IP'), sg.InputText(ip)],
+            [sg.Checkbox('Window & border less', default=borderless)],
             [sg.Button('Launch')] ]
 
 window = sg.Window('VRPhone', layout)
@@ -27,8 +41,17 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel':	# if user closes window or clicks cancel
         break
     ip = values[0]
+    borderless = values[1]
+
     window.close()
-    launch(ip)
+
+    f = open("settings.txt", "w")
+    f.write(ip)
+    f.write("\n")
+    f.write(str(borderless))
+    f.close()
+
+    launch(ip, borderless)
     break
 
 window.close()
